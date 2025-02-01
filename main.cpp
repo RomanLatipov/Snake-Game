e0e0cb7054236f2278e6d9014b917f2ab2b4777c
RomanLatipov <61292132+RomanLatipov@users.noreply.github.com>
2025-02-01 03:20:15 +0000

    create cls window and board

--- main.cpp
+++ main.cpp
@@ -2,12 +2,49 @@
 using namespace std;
 
 bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y;
 
-void Setup() {
-  gameOver = false;
+void setup() {
+    gameOver = false;
 }
+void draw() {
+    system("cls");
 
+    for (int i = 0; i < width; i++) {
+        cout << "#";
+    }
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#";
+
+            cout << " ";
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+     for(int i=0; i<width; i++) {
+       cout << "#";
+     }
+}
+void input() {
+
+}
+void logic() {
+
+}
 int main() {
-  Setup();
-  std:: cout <<"Hello world!" <<std::endl;
+    setup();
+    while (!gameOver) {
+        draw();
+        input();
+        logic();
+    }
+    cout << "Hello world!" << endl;
 }
